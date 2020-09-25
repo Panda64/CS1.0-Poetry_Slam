8088cd8ea83940924df02280e3f1e7dc996f9717
Panda64 <JaylenSchelb@yahoo.com>
2020-09-24 22:08:49 -0700

    Created initial get_file_lines function

--- program.py
+++ program.py
@@ -1,4 +1,11 @@
 # Name - Jaylen Schelb
 # Program - Poetry Slam
 
+def get_file_lines(filename):
+    infile = open(filename, "r")
+    data = infile.readlines()
+    
+    return data
 
+test = get_file_lines('poem.txt')
+print(test)
