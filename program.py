bcf138e0acac73a452359c09ebcb7edc39cee08e
Panda64 <JaylenSchelb@yahoo.com>
2020-09-29 23:54:51 -0700

    Finished custom function
    This function will switch around the first and second half of each line of the poem. This creates the possibility of funny sentences/phrases, along with ones that make little to no sense.

--- program.py
+++ program.py
@@ -3,6 +3,8 @@
 
 from random import choice
 
+print("")
+
 def get_file_lines(filename):
     infile = open(filename, "r")
     raw_data = infile.readlines()
@@ -16,11 +18,17 @@ data = get_file_lines("poem.txt")
 
 def lines_printed_backwards(lines_list):
     lines_list.reverse()
-    line_num = len(lines_list)
 
-    for i in lines_list:
-        print(f"{line_num} {i}")
-        line_num -= 1
+    no_count = lines_list.count("")
+    raw_line_num = len(lines_list)
+    line_num = raw_line_num - no_count
+
+    for i in lines_list: 
+        if i == "":
+            print(i)
+        else:
+            print(f"{line_num} {i}")
+            line_num -= 1
         
 # lines_printed_backwards(data)
 
@@ -32,11 +40,17 @@ def lines_printed_random(lines_list):
 
 def lines_printed_custom(lines_list):
     for i in lines_list:
-        first_half = i[:len(i)//2]
-        second_half = i[len(i)//2:]
+        i = i.split()
+        i_length = len(i)
+        half_i_length = i_length // 2
 
-        print(second_half + first_half)
+        first_half_list = i[0:half_i_length]
+        second_half_list = i[half_i_length:i_length]
 
-# lines_printed_custom(data)
+        first_half = " ".join(first_half_list)
+        second_half = " ".join(second_half_list)
 
-print(data)
+        print(second_half + " " + first_half)
+
+lines_printed_custom(data)
+
